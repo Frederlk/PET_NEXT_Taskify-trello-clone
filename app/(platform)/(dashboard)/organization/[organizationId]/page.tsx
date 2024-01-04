afb974dcb06efd177545b79d51e97cc7ef7e1a2b
Sergey Petrinich <petrinich.sergey@gmail.com>
2024-01-04 18:33:17 +0300

    env setup | folders setup | marketing page | auth | organization page

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,9 @@
+import { OrganizationSwitcher, auth } from '@clerk/nextjs';
+
+const OrganizationIdPage = () => {
+  const { userId, orgId } = auth();
+
+  return <div>{/* <OrganizationSwitcher hidePersonal /> */}</div>;
+};
+
+export default OrganizationIdPage;
