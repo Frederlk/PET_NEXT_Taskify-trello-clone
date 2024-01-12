8943b7d412bf42e1620f8e65960fb00908b736e6
Sergey Petrinich <petrinich.sergey@gmail.com>
2024-01-12 16:08:52 +0300

    actions | useAction | form components

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,23 @@
-import { OrganizationSwitcher, auth } from '@clerk/nextjs';
+import { Suspense } from 'react';
 
-const OrganizationIdPage = () => {
-  const { userId, orgId } = auth();
+import { Separator } from '@/components/ui/separator';
 
-  return <div>{/* <OrganizationSwitcher hidePersonal /> */}</div>;
+import { Info } from './_components/info';
+import { BoardList } from './_components/board-list';
+// import { checkSubscription } from "@/lib/subscription";
+
+const OrganizationIdPage = async () => {
+  // const isPro = await checkSubscription();
+
+  return (
+    <div className="w-full mb-20">
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList />
+      </div>
+    </div>
+  );
 };
 
 export default OrganizationIdPage;
---
+++ components/hint.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react';
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from './ui/tooltip';
+
+interface HintProps {
+  children: ReactNode;
+  description: string;
+  side?: 'left' | 'right' | 'top' | 'bottom';
+  sideOffset?: number;
+}
+
+export const Hint = ({
+  children,
+  description,
+  side,
+  sideOffset,
+}: HintProps) => {
+  return (
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger>{children}</TooltipTrigger>
+        <TooltipContent
+          side={side}
+          sideOffset={sideOffset}
+          className="text-xs max-w-[220px] break-words"
+        >
+          {description}
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
+  );
+};
