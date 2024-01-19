0d7c01bf7473242ffa95d9c3ea4f82750a153548
Sergey Petrinich <petrinich.sergey@gmail.com>
2024-01-19 19:19:11 +0300

    card modal | card actions

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,12 +1,18 @@
+import { Toaster } from 'sonner';
 import { ClerkProvider } from '@clerk/nextjs';
 import type { ReactNode } from 'react';
-import { Toaster } from 'sonner';
+
+import { ModalProvider } from '@/components/providers/modal-provider';
+import { QueryProvider } from '@/components/providers/query-provider';
 
 const PlatformLayout = ({ children }: { children: ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster />
-      {children}
+      <QueryProvider>
+        <Toaster />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
